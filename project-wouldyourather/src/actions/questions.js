c0ee17021c1df8bbf81c16052bc5854501e8d6d5
clement <chunchieh.liang@coolbitx.com>
2020-09-28 17:27:26 +0800

    chore: add ADD_QUESTION and ADD_ANSWER actions in questions

--- project-wouldyourather/src/actions/questions.js
+++ project-wouldyourather/src/actions/questions.js
@@ -1,8 +1,61 @@
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+import { _saveQuestion, _saveQuestionAnswer } from "../utils/_DATA";
+
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
+export const ADD_QUESTION = "ADD_QUESTION";
+export const ADD_ANSWER = "ADD_ANSWER";
 
 export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  };
+}
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  };
+}
+
+export function handleAddQuestion({
+  optionOneText,
+  optionTwoText,
+  authedUser,
+}) {
+  return (dispatch) => {
+    dispatch(showLoading());
+
+    return _saveQuestion({ optionOneText, optionTwoText, author: authedUser })
+      .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
+
+function addAnswer({ authedUser, questionId, answer }) {
+  return {
+    type: ADD_ANSWER,
+    authedUser,
+    questionId,
+    answer,
+  };
+}
+
+export function handleAddAnswer({ authedUser, questionId, answer }) {
+  return (dispatch) => {
+    dispatch(showLoading());
+    dispatch(
+      addAnswer({
+        authedUser: authedUser,
+        questionId: questionId,
+        answer: answer,
+      })
+    );
+    return _saveQuestionAnswer({
+      authedUser: authedUser,
+      qid: questionId,
+      answer: answer,
+    }).then(() => dispatch(hideLoading()));
   };
 }
